df4c8f76c46d0bbd345f3e8a2f3d41e02a26059b
oshou <adf1985adf@gmail.com>
2018-05-12 23:40:07 +0900

    post

---
+++ sample5.py
@@ -0,0 +1,8 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(fruits)
+print(type(fruits))
+print(fruits.sort())
+print(fruits.append('grape'))
+print(fruits.count('apple'))
+print(fruits.count('tangerine'))
+print(fruits.index('banana'))
